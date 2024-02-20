2fd31a1eab6c471810b2e6a8d5d74eb31bedeb7d
Eason Yilin  Zhou <ut21586@bristol.ac.uk>
2024-02-20 22:18:10 +0000

    update

---
+++ number.py
@@ -0,0 +1,6 @@
+#type conversation
+# exp: a = float(1) the print should be 1.0
+#random number:
+import random
+
+print(random.randrange(1, 10))
---
+++ strings.py
@@ -0,0 +1,10 @@
+a = """
+    it ssosososososo great,
+    to see you and wish you,
+    all the best.
+    """
+
+print(a)
+
+
+
