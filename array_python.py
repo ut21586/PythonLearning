8a75186325e43c2339f40e3c48d74c9f6fb75bf6
Eason Yilin  Zhou <ut21586@bristol.ac.uk>
2024-03-16 12:26:37 +0000

    update

---
+++ array_python.py
@@ -0,0 +1,5 @@
+#this page shows you how to use LISTS as ARRAYS, however, to work with arrays in python you will have to import a library, like the NumPy library
+#arrays are used to store multiple values in one single variable
+
+cars = ["Ford", "volvo", "bmw"]
+print(cars)
---
+++ lambda.py
@@ -0,0 +1,16 @@
+#a lambda function is a small anonymous function
+#a lambda function can take any number of arguements, but can only have 1 expression
+#lambda syntax: lamda arguments: expression
+
+l = lambda a: a + 10
+print(l(5)) #should be print 15
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#why use lambda functions
+
+
