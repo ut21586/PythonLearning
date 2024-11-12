222d5fee5dc9f03187a7b44475f39acc14bc3c6d
Eason Yilin  Zhou <ut21586@bristol.ac.uk>
2024-11-12 14:55:12 +0000

    update

--- introduction_to_algorithms/insertion_sort.py
+++ introduction_to_algorithms/insertion_sort.py
@@ -4,3 +4,15 @@ Translate insertion sort pseudocode to python.
 Aim to insert poker card from right hand to left hand with correct order.
 """
 
+# Define the list A with any integer values
+A = [5, 2, 4, 6, 1, 3]
+
+for i in range(1, len(A)):
+    key = A[i]
+    j = i - 1
+    while j >= 0 and A[j] > key:
+        A[j + 1] = A[j]
+        j = j - 1
+    A[j + 1] = key
+
+print(A)
