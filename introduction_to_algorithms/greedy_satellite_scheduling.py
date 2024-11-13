898f9c62c5fd6c9143171b514f9cc165d00096e6
Eason Yilin  Zhou <ut21586@bristol.ac.uk>
2024-11-13 18:00:43 +0000

    update

---
+++ introduction_to_algorithms/greedy_satellite_scheduling.py
@@ -0,0 +1,9 @@
+"""
+Greedy algorithm for satellite scheduling problem
+Use greedy algorithm to find each earlist finish time stage 
+Combine stages into a subset as the maximum compatible subset
+
+Input: an array r of n requests
+output: a maximum compatible subset of r
+"""
+
